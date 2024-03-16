443f9992bc07c89302c6519e29dc309635b3abfb
chenhui.han <chenhui.han@tonghesoft.com>
2024-03-16 18:41:45 +0800

    增加20s无操作进入深度睡眠模式

--- src/Main.cpp
+++ src/Main.cpp
@@ -5,7 +5,13 @@
 
 #include "ReaderPosRecord.h"
 
+#include "esp_sleep.h"
+
 SPIClass spi = SPIClass(VSPI);
+
+int operate_times = 0;
+// 限制最大未操作次数 大约20s
+int sleep_times = 40;
 
 void setup() {
     Serial.begin(115200);
@@ -29,6 +35,18 @@ void action() {
     if (key_flag = get_key()) {
         doAction(key_flag);
         reset_key();
+        // 有操作重置操作次数
+        operate_times = 0;
+    }
+    // 记录无操作时间，
+    operate_times ++;
+    // 如果超过最大无操作次数，40*0.5s=20s，大约20s无操作；
+    // 当然有可能被其他操作阻塞，例如文件传输，先不进行处理防止中断文件传输
+    if (operate_times > sleep_times) {
+        // 休眠,33端口高电平恢复
+        esp_sleep_enable_ext1_wakeup(1ull<<33, ESP_EXT1_WAKEUP_ANY_HIGH);
+        esp_deep_sleep_start();
+        operate_times = 0;
     }
     delay(500);
 }
